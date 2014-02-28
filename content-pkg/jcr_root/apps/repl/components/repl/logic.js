9d985d3cca580b4ed6b078efb9b1d65d60381a69
Marius-Andrei Danila <mdanila@adobe.com>
2014-02-28 12:35:49 +0100

    Redesigned main page

--- content-pkg/jcr_root/apps/repl/components/repl/logic.js
+++ content-pkg/jcr_root/apps/repl/components/repl/logic.js
@@ -1,12 +1,7 @@
-
-
 use(function() {
+    
     return {
-        message: function() { return 'hello'; },
-        list: ['apples', 'oranges', 'bananas'],
-        attributes : {'class' : 'myClass', 'id' : 'myId'}
+        message: 'World!'
     };
+    
 });
-
-
-  
